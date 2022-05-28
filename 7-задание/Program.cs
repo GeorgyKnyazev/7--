0a2eaa6e2b1580a2cee050db36684a3f23f22a01
Георгий Князев <saint_georgy@yahoo.com>
2022-05-28 14:07:54 +0300

    Добавьте файлы проекта.

---
+++ 7-задание/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _7_задание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество человек в очереди: ");
+            int numberOfPeopleInTheQueue = Convert.ToInt32(Console.ReadLine());
+            int durationOfreception = 10;
+            int waitingHours = (numberOfPeopleInTheQueue * durationOfreception) / 60;
+            int minutesOfWaiting = (numberOfPeopleInTheQueue * durationOfreception) % 60;
+            Console.WriteLine($"Вы должны стоять в очереди {waitingHours} часа и {minutesOfWaiting} минут");
+        }
+    }
+}
